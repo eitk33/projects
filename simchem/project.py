6565a1a5fd397612c1a368feadb64baeea85487a
eitk33 <eelchai@gmail.com>
2022-12-08 16:35:58 +0200

    simchem after review

--- simchem/project.py
+++ simchem/project.py
@@ -3,15 +3,17 @@ import sys
 import requests
 import numpy as np
 
-els = []
+elements = []
 response = requests.get(
     'https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json').json()
 """
 Els used to prepare a list of elements with their characteristics taken from response
 """
-for item in response['elements']:
-    els.append(item)
 
+for item in response['elements']:
+    elements.append(item)
+
+R = 0.082057366080960
 
 def main():
     """
@@ -29,31 +31,31 @@ def main():
 
     while True:
         x = input('action: ')
-        match x:
+        match x.lower():
             case 'molarmass':
-                s = input('element: ')
-                print(molarmass(s))
+                molecule = input('element: ')
+                print(f'{molarmass(molecule):.2f}')
             case 'missing':
-                s = input('element: ')
-                w = input('total weight: ')
-                print(missing(s, w))
+                molecule = input('element: ')
+                weight = input('total weight: ')
+                print(missing(molecule, weight))
             case 'ige':
-                mydict = dict.fromkeys(['V', 'P', 'T', 'n'])
+                ige_variables = dict.fromkeys(['V', 'P', 'T', 'n'])
                 print('Please insert known values with precaution to units (L for volume, atm for pressure, and Kelvin'
                       ' for temperature')
-                for key in mydict.keys():
-                    a = input(f'{key}: ')
-                    try:
-                        mydict[key] = float(a)
-                    except ValueError:
-                        mydict[key] = a
-                mydict['Rr'] = 0.082057366080960
-                print(ige(mydict))
+                for key in ige_variables.keys():
+                    unknown_variable = input(f'{key}: ')
+                    if unknown_variable.isdigit():
+                        ige_variables[key] = float(unknown_variable)
+                    else:
+                        ige_variables[key] = unknown_variable
+
+                print(ige(ige_variables))
             case 'exit':
                 sys.exit(0)
 
 
-def molarmass(s):
+def molarmass(molecule):
     """
 
     _Return the MW (molar mass) of a given compound_
@@ -69,25 +71,24 @@ def molarmass(s):
     respond to a valid atom, there will not be an error message, and the function will ignore this value\n
     return type: int
     """
-    global els
     mass = 0
 
-    if elements := re.findall(r'(?:[A-Z][a-z]?)(?:\d?\d)?', s):
-        for elem in elements:
-            if sub := re.search(r'([A-Z][a-z]?)(\d?\d)?', elem):
-                for items in els:
-                    if sub[1] == items['symbol']:
-                        if not sub[2]:
-                            mul = 1
+    if parsed_elements := re.findall(r'(?:[A-Z][a-z]?)(?:\d?\d)?', molecule):
+        for elem in parsed_elements:
+            if atoms := re.search(r'([A-Z][a-z]?)(\d?\d)?', elem):
+                for items in elements:
+                    if atoms[1] == items['symbol']:
+                        if not atoms[2]:
+                            number_of_atoms = 1
                         else:
-                            mul = int(sub[2])
-                        mass += float(items['atomic_mass']) * mul
+                            number_of_atoms = int(atoms[2])
+                        mass += float(items['atomic_mass']) * number_of_atoms
         return mass
     else:
-        return 'not a valid format'
+        return 'Input must be in the format of: CAPITAL_CHAR(char - optional)(numer - optional). See Readme.me file'
 
 
-def missing(s, v):
+def missing(molecule, weight):
     """
     _Return the atomic symbol of an unknown element in a given compound if total weight is known_\n
     param s: Molecular formula with unknown atom represented by X. case-sensitive. has to be in the format:
@@ -104,59 +105,61 @@ def missing(s, v):
     return: either the symbol of the unknown atom, or an error message\n
     rtype: str or an int (depend on the usage) in the case of a successful attempt, str in the case of an error
     """
-    global els
-    known = molarmass(s)
-    un = float(v) - known
 
+    known = molarmass(molecule)
+    weight_of_unknown_atoms = float(weight) - known
+    if not 'x' in molecule.lower():
+        return 'Need to notate unknown element by "x"'
     try:
-        if a := re.search(r'(?:[X|x](\d?\d)?)', s):
-            a = int(a[1])
+        if atoms := re.search(r'(?:[X|x](\d?\d)?)', molecule):
+            number_of_atoms = int(atoms[1])
     except TypeError:
-        a = 1
+        number_of_atoms = 1
 
-    unn = np.format_float_positional(un / a, precision=3, unique=False, fractional=True, trim='k')
-    for items in els:
+    normalized_atom_weight = np.format_float_positional(weight_of_unknown_atoms / number_of_atoms, precision=2,
+                                                        unique=False, fractional=True, trim='k')
+    for items in elements:
+        if normalized_atom_weight == np.format_float_positional(items['atomic_mass'], precision=2,
+                                                          unique=False, fractional=True, trim='k'):
+            elem = items['symbol']
+            return elem
+    return 'none found'
 
-        if float(unn) == items['atomic_mass']:
-            ele = items['symbol']
-            return ele
-    return 'non found'
 
-
-def ige(mydict):
+def ige(ige_variables):
     """
     _Calculating the ideal gas equation (ige) for one unknown parameter_\n
     This function get passed in a dict from the main(). This dict contains the user's input about
     the parameters of the ige, including one unknown value that needs to be specified by 'x'.\n
-    param mydict: dict object\n
+    param ige_variables: dict object\n
     raise TypeError: if any other character other than 'x' is submitted, or more than 1 x is submitted\n
     return: str of resulted float
     """
 
-    res = 0
-    v = mydict['V']
-    p = mydict['P']
-    t = mydict['T']
-    n = mydict['n']
-    r = mydict['Rr']
-    for parameter in mydict.items():
+    result = 0
+    v = ige_variables['V']
+    p = ige_variables['P']
+    t = ige_variables['T']
+    n = ige_variables['n']
 
-        if parameter[1] == 'x':
+    list_of_unknown = [x for x in ige_variables if not ige_variables[x]]
+    if len(list_of_unknown) > 1:
+        return "Can't calculate 2 variables"
+    for parameter in ige_variables.items():
+        if parameter[1] == 'x' or not parameter[1]:
             match (parameter[0]):
                 case 'V':
-                    res = r * n * t / p
+                    res = R * n * t / p
                 case 'n':
-                    res = (p * v) / (r * t)
+                    res = (p * v) / (R * t)
                 case 'P':
-                    res = r * n * t / v
+                    res = R * n * t / v
                 case 'T':
-                    res = r * n / (p * v)
+                    res = R * n / (p * v)
             return f'{parameter[0]} = {res:.3f}'
 
-        elif not type(parameter[1]) == float:
-            raise ValueError(f"{parameter[0]} value must be 'x' or a number")
-
-    return
+        elif not isinstance(parameter[1], float):
+            return f"{parameter[0]} value must be 'x/None' or a number"
 
 
 if __name__ == '__main__':
